82ed21f0af62ab084653a3cbf6791ba024f267ce
Gabriel Lumbi <gabriel.lumbi@gmail.com>
2021-06-26 20:05:11 +0900

    Add control instructions and fix platform in stage 1

---
+++ src/assets/maps/keys.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="keys" tilewidth="16" tileheight="16" tilecount="768" columns="2">
+ <image source="../tiles/keys.png" width="32" height="48"/>
+</tileset>
